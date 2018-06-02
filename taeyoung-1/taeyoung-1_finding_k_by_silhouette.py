cf970aaa0f9b5200a9878c2b1503c326d459d56f
taeyoung-1 <taeyoung210@gmail.com>
2018-06-02 21:16:46 +0900

    'add'

---
+++ taeyoung-1/taeyoung-1_finding_k_by_silhouette.py
@@ -0,0 +1,159 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = -0.3036820614007026
+Traceback (most recent call last):
+  File "C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py", line 111, in <module>
+    S = Silhouette(centroid_values, assignment_values, data)
+  File "C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py", line 35, in Silhouette
+    a_i_list_sum.append(sum(a_i_list) / (count - 1))
+UnboundLocalError: local variable 'a_i_list' referenced before assignment
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = -0.3255784430423964
+when k = 3, s(i) = 0.3002820285574777
+when k = 4, s(i) = -0.2959686327171705
+The value of k : 3
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.18849919952162927
+when k = 3, s(i) = -0.06498273574417883
+The value of k : 2
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.18369396340167693
+when k = 3, s(i) = 0.30092396681131994
+when k = 4, s(i) = 0.6560223876706217
+when k = 5, s(i) = 0.6445570785729117
+The value of k : 4
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.1870036794739372
+when k = 3, s(i) = 0.6345687366300444
+when k = 4, s(i) = -0.2490487591986288
+The value of k : 3
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.4214879084732849
+when k = 3, s(i) = 0.31549645391970726
+The value of k : 2
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.2064567924610437
+when k = 3, s(i) = -0.008867770287694846
+The value of k : 2
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.18958320099283374
+when k = 3, s(i) = 0.518677012376893
+when k = 4, s(i) = 0.38969468944854674
+The value of k : 3
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = -0.3143088501473692
+when k = 3, s(i) = 0.30042456505971243
+when k = 4, s(i) = -0.019303915656564585
+The value of k : 3
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.4508982859451226
+when k = 3, s(i) = 0.7247500582818014
+when k = 4, s(i) = 0.8648429486338052
+when k = 5, s(i) = 0.8735472825211593
+when k = 6, s(i) = 0.8782238485078038
+when k = 7, s(i) = 0.8941908744981504
+when k = 8, s(i) = 0.8898298381030012
+The value of k : 7
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.4476197189749168
+when k = 3, s(i) = 0.725014064845286
+when k = 4, s(i) = 0.8645894819280591
+when k = 5, s(i) = 0.8719778638281112
+when k = 6, s(i) = 0.8740239969400602
+when k = 7, s(i) = 0.8898897587317969
+when k = 8, s(i) = 0.887524836249468
+The value of k : 7
+
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.45297324442651155
+when k = 3, s(i) = 0.7273384059325086
+when k = 4, s(i) = 0.8665849533904989
+when k = 5, s(i) = 0.8742329668258121
+when k = 6, s(i) = 0.8867366643274924
+when k = 7, s(i) = 0.8871288758415449
+when k = 8, s(i) = 0.8902029919746406
+
+Warning (from warnings module):
+  File "C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py", line 37
+    a_i = sum(a_i_list_sum)/ count
+RuntimeWarning: invalid value encountered in long_scalars
+Traceback (most recent call last):
+  File "C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py", line 111, in <module>
+    S = Silhouette(centroid_values, assignment_values, data)
+  File "C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py", line 38, in Silhouette
+    b_i = min(b_i_list_sum)
+ValueError: min() arg is an empty sequence
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.4517040826524247
+when k = 3, s(i) = 0.7245207210100322
+when k = 4, s(i) = 0.8627312875050578
+when k = 5, s(i) = 0.8705357968802225
+when k = 6, s(i) = 0.8747919119321055
+when k = 7, s(i) = 0.8838131356427947
+when k = 8, s(i) = 0.9005147971881796
+when k = 9, s(i) = 0.8874654435327398
+The value of k : 8
+
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.44608265028987837
+when k = 3, s(i) = 0.7266292413135704
+when k = 4, s(i) = 0.8650319828894483
+when k = 5, s(i) = 0.8731787684965815
+when k = 6, s(i) = 0.8279114676198142
+The value of k : 5
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.46048937556519365
+when k = 3, s(i) = 0.7310307313037631
+when k = 4, s(i) = 0.8703701675987127
+when k = 5, s(i) = 0.8755290162714783
+when k = 6, s(i) = 0.8836932500804502
+when k = 7, s(i) = 0.8855042909083649
+when k = 8, s(i) = 0.8962784651738515
+when k = 9, s(i) = 0.892129519852805
+The value of k : 8
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+Traceback (most recent call last):
+  File "C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py", line 83, in <module>
+    S = Silhouette(centroid_values, assignment_values, data)
+  File "C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py", line 40, in Silhouette
+    b_i_list_sum.append(sum(b_i_in_c2)/(len(_data["cluster"][c2])))
+TypeError: object of type 'numpy.int64' has no len()
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.7228670165592146
+when k = 3, s(i) = 0.7975460744063713
+when k = 4, s(i) = 0.9225791952490043
+when k = 5, s(i) = 0.8110206280522981
+The value of k : 4
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.72515220076363
+when k = 3, s(i) = 0.7894623042174581
+when k = 4, s(i) = 0.9213372595648298
+when k = 5, s(i) = 0.6735069266831135
+The value of k : 4
+>>> 
+========== RESTART: C:\Users\윤도영\FaceClustering\taeyoung-1\my_k.py ==========
+when k = 2, s(i) = 0.7254390841882322
+when k = 3, s(i) = 0.7917662839898797
+when k = 4, s(i) = 0.9214452647486364
+when k = 5, s(i) = 0.7971962455075265
+The value of k : 4
+>>> 
