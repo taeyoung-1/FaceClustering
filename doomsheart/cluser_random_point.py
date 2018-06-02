155005c546de00d350c151fa553264480b0bfc79
Joonho Wohn <doomsheart@naver.com>
2018-06-02 19:47:23 +0900

    completo

--- doomsheart/cluser_random_point.py
+++ doomsheart/cluser_random_point.py
@@ -8,7 +8,7 @@ orbit_arr = []
 x_list = [2, 8]
 y_list = [2, 8]
 sign = [-1, 1]
-for i in range(10):
+for i in range(1000):
     orbit_arr.append(((x_list[(random.randrange(0, 2))] + sign[(random.randrange(0, 2))] * pow(random.random(), 2))
                       , (y_list[(random.randrange(0, 2))] + sign[(random.randrange(0, 2))] * pow(random.random(), 2))))
             
@@ -38,7 +38,7 @@ def David_Bouldin_index(_centroid, _assignment, _data):
                 if max_value < (sig_i + sig_j) / d_ci_cj:
                     max_value = (sig_i + sig_j) / d_ci_cj
         result += max_value
-    print(result)
+    # print(result)
     return result
 
 def silhouette(_centroid, _assignment, _points, _i):
@@ -47,7 +47,13 @@ def silhouette(_centroid, _assignment, _points, _i):
         _curr_cluster = _assignment[__i]
         _x_point = _points[__i]
         _same_cluster_points = [p for a,p in zip(_assignment, _points) if a == _curr_cluster]
-        a_val = sum([distance(_s, _x_point) for _s in _same_cluster_points]) / (len(_same_cluster_points) - 1)
+        try:
+            a_val = sum([distance(_s, _x_point) for _s in _same_cluster_points]) / (len(_same_cluster_points) - 1)
+        except:
+            print("======================a_val error======================")
+            print(_centroid, _assignment, _points, _i)
+            a_val = 1
+
         # print("a_val is " + str(a_val))
         return a_val
     # function b is mean distance between x(i) and data in proximate cluster.
@@ -56,22 +62,27 @@ def silhouette(_centroid, _assignment, _points, _i):
         _x_point = _points[__i]
         _dis_list = [distance(_centroid[_curr_cluster], _c) for _c in _centroid]
         # print(_dis_list)
-        _proxmiate_cluter = -1
+        _proximate_cluster = -1
         _min_dis = sum(_dis_list)
         for i in range(len(_dis_list)):
             if (_dis_list[i] != 0) and (_min_dis > _dis_list[i]):
-                _proxmiate_cluter = i
+                _proximate_cluster = i
                 _min_dis = _dis_list[i]
-        if _proxmiate_cluter == -1:
+        if _proximate_cluster == -1:
             return 0
-        # print(_curr_cluster, _proxmiate_cluter)
-        _proxmiate_cluster_points = [p for a,p in zip(_assignment, _points) if a == _proxmiate_cluter]
-        b_val = sum([distance(_s, _x_point) for _s in _proxmiate_cluster_points]) / (len(_proxmiate_cluster_points) - 1)
+        # print(_curr_cluster, _proximate_cluster)
+        _proximate_cluster_points = [p for a,p in zip(_assignment, _points) if a == _proximate_cluster]
+        try :
+            b_val = sum([distance(_s, _x_point) for _s in _proximate_cluster_points]) / (len(_proximate_cluster_points) - 1)
+        except:
+            print("======================a_val error======================")
+            print(_centroid, _assignment, _points, _i)
+            b_val = 1
         return b_val
     a_i = a(_i)
     b_i = b(_i)
-    sil = b_i - a_i / max(a_i, b_i)
-    print(a_i,b_i, max(a_i, b_i), sil)
+    sil = (b_i - a_i) / max(a_i, b_i)
+    # print(a_i,b_i, max(a_i, b_i), sil)
     
     # print("silhouette is " + str(sil))
     return sil
@@ -95,7 +106,7 @@ def cluster_random_points(k_, points_):
 
 def evaluate_cluster(_centroid, _assignment, _data):
     result = sum([silhouette(_centroid,_assignment,_data,i) for i in range(len(_assignment))]) / len(_assignment)
-    print(result)
+    # print(result)
     return result
 
 # # TODO
@@ -129,37 +140,45 @@ def evaluate_cluster(_centroid, _assignment, _data):
 # make algorithm that find accurate cluster and evaluate db value so that find appropriate k
 # silhouette(_centroid, _assignment, _points, _i):
 k = 1
-result_cluster = cluster_random_points(k, orbit_arr)
+result_centroid, result_assignment = cluster_random_points(k, orbit_arr)
+result_evaluated = evaluate_cluster(_centroid=result_centroid, _assignment=result_assignment, _data=orbit_arr)
+
 while True:
     print("k is " + str(k))
-    optimized_k_cluster_centroid_and_assignment = cluster_random_points(k, orbit_arr)
-    cluster_evaluated = evaluate_cluster(_centroid=result_cluster[0],
-                                            _assignment=result_cluster[1], _data=orbit_arr)
-    curr_evaluated = cluster_evaluated
-    for i in range(10):
-        clustering_result = cluster_random_points(k, orbit_arr)
-        curr_evaluated = evaluate_cluster(_centroid=optimized_k_cluster_centroid_and_assignment[0], 
-                                            _assignment=optimized_k_cluster_centroid_and_assignment[1], _data=orbit_arr)
-        if (cluster_evaluated < curr_evaluated):
-            print("changed. cluster_evaluated is " + str(cluster_evaluated) + " and curr_evaluated is " + str(curr_evaluated))
-            optimized_k_cluster_centroid_and_assignment = clustering_result
-            cluster_evaluated = curr_evaluated
+    optimized_centroid, optimized_assignment = cluster_random_points(k, orbit_arr)
+    optimized_evaluated = evaluate_cluster(_centroid=optimized_centroid,
+                                         _assignment=optimized_assignment,
+                                         _data=orbit_arr)
+    curr_evaluated = optimized_evaluated
+    for i in range(9):
+        tmp_centroid, tmp_assignment = cluster_random_points(k, orbit_arr)
+        curr_evaluated = evaluate_cluster(_centroid=tmp_centroid,
+                                          _assignment=tmp_assignment,
+                                          _data=orbit_arr)
+        if (optimized_evaluated < curr_evaluated):
+            print("changed. cluster_evaluated is " + str(optimized_evaluated) + " and curr_evaluated is " + str(curr_evaluated))
+            optimized_centroid = tmp_centroid
+            optimized_assignment = tmp_assignment
+            optimized_evaluated = curr_evaluated
     k += 1
-    if cluster_evaluated < evaluate_cluster(_centroid=result_cluster[0],
-                                            _assignment=result_cluster[1], _data=orbit_arr):
+    cmp_cluster_evaluated = evaluate_cluster(_centroid=optimized_centroid,
+                                             _assignment=optimized_assignment,
+                                             _data=orbit_arr)
+    if result_evaluated > optimized_evaluated :
         break
     else:
         print(k)
-        result_cluster = optimized_k_cluster_centroid_and_assignment
-        print(cluster_evaluated)
+        result_centroid, result_assignment = optimized_centroid, optimized_assignment
+        result_evaluated = optimized_evaluated
+        print(result_evaluated)
 k -= 2
 print("result k is " + str(k))
 data = {"x": [], "y": [], "cluster": []}
-print(result_cluster[1])
-for i in range(len(result_cluster[1])):
+print(result_assignment)
+for i in range(len(result_assignment)):
     data["x"].append(orbit_arr[i][0])
     data["y"].append(orbit_arr[i][1])
-    data["cluster"].append(result_cluster[1][i])
+    data["cluster"].append(result_assignment[i])
 
 df = pd.DataFrame(data)
 sns.lmplot("x", "y", data=df, fit_reg=False, size=6, hue="cluster", legend=False)
